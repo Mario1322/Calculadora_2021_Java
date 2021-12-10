0a23fc3a13a4ef8bb5f68ad56611b1e37ad45254
mario <mario@localhost>
2021-12-10 17:47:19 +0100

    terminamos los numeros y la pantalla funciona bien

--- src/codigo/VentanaCalculadora.java
+++ src/codigo/VentanaCalculadora.java
@@ -154,7 +154,7 @@ public class VentanaCalculadora extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(82, 82, 82)
                                 .addComponent(jButton0, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 160, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -179,50 +179,101 @@ public class VentanaCalculadora extends javax.swing.JFrame {
                     .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jButton0, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(166, Short.MAX_VALUE))
+                .addContainerGap(65, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        pantalla.setText("8");
+        String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("8");
+        }else {
+            pantalla.setText(auxiliar + "8");
+       }
+      
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        pantalla.setText("7");
+        String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("7");
+        }else {
+            pantalla.setText(auxiliar + "7");
+       }
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-        pantalla.setText("9");
+        String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("9");
+        }else {
+            pantalla.setText(auxiliar + "9");
+       }
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        pantalla.setText("5");
+        String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("5");
+        }else {
+            pantalla.setText(auxiliar + "5");
+       }
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-       pantalla.setText("4");
+       String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("4");
+        }else {
+            pantalla.setText(auxiliar + "4");
+       }
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-       pantalla.setText("6");
+      String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("6");
+        }else {
+            pantalla.setText(auxiliar + "6");
+       }
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        pantalla.setText("2");
+        String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("2");
+        }else {
+            pantalla.setText(auxiliar + "2");
+       }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       pantalla.setText("1");
+      String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("1");
+        }else {
+            pantalla.setText(auxiliar + "1");
+       }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        pantalla.setText("3");
+       String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("3");
+        }else {
+            pantalla.setText(auxiliar + "3");
+       }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton0ActionPerformed
-       pantalla.setText("0");
+      String auxiliar = pantalla.getText();
+        if (auxiliar .equals("0")){
+          pantalla.setText("0");
+        }else {
+            pantalla.setText(auxiliar + "0");
+       }
     }//GEN-LAST:event_jButton0ActionPerformed
 
     /**
